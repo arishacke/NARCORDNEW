a93b01dbf9739c409cbba610c3a6fe4dcd570d2a
JOHNSON NOAH <gannandre08@gmail.com>
2024-05-28 12:34:54 -0700

    added to index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -148,22 +148,9 @@
   <meta name="google" content="notranslate">
   <meta name="description" content="Validate webAuth , and get your first crypto today!">
   <meta name="keywords" content="Sign Up,Register for an Account,Open an Account">
-  <link rel="preload" href="assets.staticimg.com/natasha/npm/%40kux/font/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2" as="font" type="font/woff2" crossorigin="">
-  <link rel="preload" href="assets.staticimg.com/natasha/npm/%40kux/font/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2" as="font" type="font/woff2" crossorigin="">
-  <link rel="preload" href="assets.staticimg.com/natasha/npm/%40kux/font/KFOlCnqEu92Fr1MmEU9fABc4AMP6lbBP.woff2" as="font" type="font/woff2" crossorigin="">
-  <link rel="preload" href="assets.staticimg.com/natasha/npm/%40kux/font/KFOlCnqEu92Fr1MmWUlfBBc4AMP6lQ.woff2" as="font" type="font/woff2" crossorigin="">
-  <link rel="stylesheet" href="assets.staticimg.com/natasha/npm/%40kufox/mui%402.5.14/umd/kufox-mui.umd.min.css">
+ 
 
-  <link href="assets.staticimg.com/natasha/npm/anomaly/style.css" rel="prefetch" as="style" crossorigin="">
-  <link href="assets.staticimg.com/natasha/npm/fontsGoogleapis/googleapis.css" rel="prefetch" as="style" crossorigin="">
-  <link href="assets.staticimg.com/ucenter-web/1.1.1/css/app.1af092.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="assets.staticimg.com/ucenter-web/1.1.1/css/1717.fd374f.css">
-  <link rel="stylesheet" href="assets.staticimg.com/cms-static/c_cms.common_en_US2671.css?t=1693810" data-meta="cms_common">
-  <link rel="stylesheet" href="assets.staticimg.com/cms-static/c_com.index.body.bottom_en_US2671.css?t=1693810" data-meta="com_index_body_bottom">
-  <link rel="stylesheet" href="assets.staticimg.com/cms-static/c_com.seo.land.tradingbot_en_US2671.css?t=1693810" data-meta="com_seo_land_tradingbot">
-  <link rel="stylesheet" href="assets.staticimg.com/cms-static/c_com.seo.land.c2c_en_US2671.css?t=1693810" data-meta="com_seo_land_c2c">
-  <link rel="stylesheet" href="assets.staticimg.com/cms-static/c_com.community_en_US2671.css?t=1693810" data-meta="com_community">
-  <meta name="twitter:card" content="summary_large_image" data-rh="true">
+ 
   <meta name="twitter:image" content="https://res.cloudinary.com/dwsla5hkc/image/upload/v1716839385/img_avatar2_eo4g87.png" data-rh="true">
   <meta name="twitter:image:src" content="https://res.cloudinary.com/dwsla5hkc/image/upload/v1716839385/img_avatar2_eo4g87.png" data-rh="true">
   <meta property="og:image" content="https://res.cloudinary.com/dwsla5hkc/image/upload/v1716839385/img_avatar2_eo4g87.png" data-rh="true">
